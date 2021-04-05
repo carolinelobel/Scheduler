a2f252c5dd09452ccd752605d74996526c44de3c
Caroline Lobel <57736173+carolinelobel@users.noreply.github.com>
2021-04-05 13:40:34 -0500

    course conflicts

--- components/CourseList.js
+++ components/CourseList.js
@@ -1,29 +1,24 @@
 import Course from './Course';
 import termSelector from './termSelector';
+import CourseSelector from './CourseSelector';
 import React, { useState } from 'react';
 import { ScrollView, StyleSheet, View } from 'react-native';
 import TermSelector from './termSelector';
+import { getCourseNumber, getCourseTerm, hasConflict, terms } from '../utils/course.js';
 
 
 const termMap = {F: 'Fall', W: 'Winter', S: 'Spring'};
-const terms = Object.values(termMap);
 
-const getCourseTerm = course => (
-    termMap[course.id.charAt(0)]
-);
+
 
 const CourseList = ({courses}) => {
     const [selectedTerm, setSelectedTerm] = useState('Fall');
     const termCourses = courses.filter(course => selectedTerm === getCourseTerm(course));
     return (
-    <View>
-        <TermSelector terms={terms} selectedTerm={selectedTerm} setSelectedTerm={setSelectedTerm}/>
         <ScrollView>
-            <View style={styles.courseList}>
-                {termCourses.map(course => <Course key={course.id} course={course} />)}
-            </View>
+            <TermSelector terms={terms} selectedTerm={selectedTerm} setSelectedTerm={setSelectedTerm} />
+            <CourseSelector courses={termCourses} />
         </ScrollView>
-    </View>
     );
 };
 
