81464e83f1f73254ae002cae819ccb5500f121e7
Caroline Lobel <57736173+carolinelobel@users.noreply.github.com>
2021-04-09 16:59:45 -0500

    store data

--- screens/ScheduleScreen.js
+++ screens/ScheduleScreen.js
@@ -4,7 +4,7 @@ import { SafeAreaView, StyleSheet, Text } from 'react-native';
 import CourseList from '../components/CourseList';
 import UserContext from '../UserContext';
 //import CourseEditScreen from './CourseEditScreen';
-import { firebase } from '../firebase';
+import { firebase } from '../utils/firebase';
 
 /*const schedule = {
   title: "CS Courses for 2018-2019",
